1063768d387d5472af03ae704603ec7ef63960d4
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-11 18:19:29 +0200

    refactor: fix index ts file errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App/App';
 import './index.css';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <App />
+	<App />,
 );
