5b451d29780713a52a4f2910adbd478654f7b636
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-12 22:32:56 +0200

    refactor: change CDN fonts by npm package npm install @fontsource/montserrat

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App/App';
 
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
