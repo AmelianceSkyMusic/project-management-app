50c4d94e9235284d080edfee40deb9f2862a03f6
Mazayw <mazayw@gmail.com>
2022-11-20 19:13:54 +0200

    feat: add base layout

--- src/App/pages/SignIn.tsx
+++ src/App/pages/SignIn.tsx
@@ -1,5 +1,83 @@
+import { NavLink } from 'react-router-dom';
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {
+	Avatar, Box, Button, Container, CssBaseline, Grid, Link, TextField, Typography,
+} from '@mui/material';
+
 export function SignIn() {
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			email: data.get('email'),
+			password: data.get('password'),
+		});
+	};
+
 	return (
-		<h1>SignIn</h1>
+		<Container
+			component="main"
+			maxWidth="xs"
+		>
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{
+					m: 1, bgcolor: 'secondary.main', fontSize: 40, viewBox: '0 0 24 24',
+				}}
+				>
+					<LockOutlinedIcon
+						inheritViewBox
+					/>
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link variant="body2" component={NavLink} to="signup">
+								Don&apos;t have an account? Sign Up
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+		</Container>
 	);
 }
