793534427c8d207795622cb7355cd098569614d3
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-11-21 22:14:05 +0200

    refactor: update main page layout

--- src/App/pages/Main/AboutUsBlock/AboutUsBlock.tsx
+++ src/App/pages/Main/AboutUsBlock/AboutUsBlock.tsx
@@ -1,11 +1,21 @@
 import Grid from '@mui/material/Unstable_Grid2';
 
+import Rajab from '~assets/images/rajab.jpg';
+
 import { Card } from './Card';
 
 export function AboutUsBlock() {
 	return (
-		<Grid container spacing={{ sm: 3, xs: 2 }}>
-			<Grid xs={4} sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+		<Grid component="section" container spacing={{ ss: 3 }}>
+			<Grid ss={12} lg={4}>
+				<Card
+					heading="Наталя"
+					description="Teach It Forward According to this principle, students study at school for free,"
+					img="https://avatars.githubusercontent.com/u/95941412?v=4"
+					alt="Bittersweet-girl"
+				/>
+			</Grid>
+			<Grid ss={12} lg={4}>
 				<Card
 					heading="Ігор"
 					description="Open Source Philosophy Our platform and education materials are publicly available on GitHub and YouTube"
@@ -13,19 +23,11 @@ export function AboutUsBlock() {
 					alt="Mazayw"
 				/>
 			</Grid>
-			<Grid xs={4} sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+			<Grid ss={12} lg={4}>
 				<Card
-					heading="Пані Наталя"
-					description="Teach It Forward According to this principle, students study at school for free,"
-					img="https://avatars.githubusercontent.com/u/95941412?v=4"
-					alt="Bittersweet-girl"
-				/>
-			</Grid>
-			<Grid xs={4} sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-				<Card
-					heading="Ameliance SkyMusic"
+					heading="Раджаб"
 					description="According to this principle, students study at school for free, but we request that they return as mentors to pass on their knowledge to the next generation of students."
-					img="https://avatars.githubusercontent.com/u/38717657?v=4"
+					img={Rajab}
 					alt="AmelianceSkyMusic"
 				/>
 			</Grid>
--- src/App/pages/Main/HeroBlock.tsx
+++ src/App/pages/Main/HeroBlock.tsx
@@ -6,25 +6,37 @@ import heroImg from '~assets/images/hero.png';
 export function HeroBlock() {
 	return (
 		<Grid
+			component="section"
 			container
-			spacing={{ sm: 3, xs: 2 }}
+			spacing={{ ss: 5, xl: 3 }}
 			sx={{
-				display: 'flex', alignItems: 'center', height: '100vh', mt: '-64px',
+				display: 'flex', flexDirection: { ss: 'column', xl: 'row' }, justifyContent: 'center', height: '100vh', mt: '-64px',
 			}}
 		>
 			<Grid
-				xs={6}
 				sx={{
 					display: 'flex',
 					flexDirection: 'column',
 					gap: 6,
-					justifyContent: 'center',
+					justifyContent: { ss: 'end', xl: 'center' },
 				}}
+				ss={12}
+				xl={6}
+				order={{ ss: 2, xl: 1 }}
 			>
 				<Typography
 					variant="h1"
+					sx={{
+						fontSize: {
+							ss: '42px', sm: '64px', xl: '88px',
+						},
+					}}
 				>
-					Project Management App
+					Project
+					<br />
+					Management
+					<br />
+					App
 				</Typography>
 				<Typography
 					variant="h5"
@@ -34,18 +46,27 @@ export function HeroBlock() {
 				</Typography>
 			</Grid>
 			<Grid
-				xs={6}
 				sx={{
-					position: 'relative',
 					display: 'flex',
-					justifyContent: 'end',
+					justifyContent: { ss: 'center', sm: 'end', xl: 'center' },
+					alignItems: { xl: 'center' },
 				}}
+				ss={12}
+				xl={6}
+				order={{ ss: 1, xl: 2 }}
 			>
 				<Box
 					sx={{
+						position: { ss: 'relative', xl: 'static' },
+						top: {
+							ss: 0, sm: '50px', lg: '100px', xl: '-200px',
+						},
+						right: {
+							ss: 0, sm: '50px', lg: '100px', xl: 0,
+						},
 						backgroundImage: `url(${heroImg})`,
-						width: 500,
-						height: 500,
+						width: '155px',
+						height: '200px',
 						backgroundRepeat: 'no-repeat',
 					}}
 				/>
