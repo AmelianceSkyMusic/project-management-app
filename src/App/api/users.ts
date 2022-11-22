110cfa422ed84177575f9aba45639a900e66643f
Mazayw <mazayw@gmail.com>
2022-11-22 12:59:05 +0200

    feat: add deleteUserById

--- src/App/api/users.ts
+++ src/App/api/users.ts
@@ -1,8 +1,10 @@
 /* eslint-disable simple-import-sort/imports */
 import { IUser } from '~/App/types/api';
+import { deleteCommon } from './common/delete';
 import { getCommon } from './common/get';
 import { putCommon } from './common/put';
 
 export const getAllUsers = () => getCommon('/users');
 export const getUserById = (userId: string) => getCommon(`/users/${userId}`);
 export const updateUserById = (userId: string, body: IUser) => putCommon(body, `/users/${userId}`);
+export const deleteUserById = (userId: string) => deleteCommon(`/users/${userId}`);
