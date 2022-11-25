0a95c6413ffc010a25270a904b23afd74f62c262
Mazayw <mazayw@gmail.com>
2022-11-25 20:45:12 +0200

    refactor: points strict types

--- src/App/api/points.ts
+++ src/App/api/points.ts
@@ -5,9 +5,25 @@ import { queryGenerator } from './common/helpers';
 import { patchCommon } from './common/patch';
 import { postCommon } from './common/post';
 
-export const getPointsByIdsListOrUserId = (listId: string[], userId: string) => getCommon(`/points?${queryGenerator(listId, userId)}`);
-export const createPoint = (body: IPointsList) => postCommon(body, '/points');
-export const updateSetOfPoints = (body: IPointsStatus[]) => patchCommon(body, '/points');
-export const getPointsByTaskId = (taskId: string) => getCommon(`/points/${taskId}`);
-export const updatePoint = (body: IPointsStatus) => patchCommon(body, '/points');
-export const deletePointById = (pointId: string) => deleteCommon(`/points/${pointId}`);
+type TAllPoints = Promise<{
+   data: IPointsList[] ;
+   status: number;
+} | {
+   data: null;
+   status: number | null;
+}>
+
+type TPoint = Promise<{
+   data: IPointsList ;
+   status: number;
+} | {
+   data: null;
+   status: number | null;
+}>
+
+export const getPointsByIdsListOrUserId = (listId: string[], userId: string) => getCommon(`/points?${queryGenerator(listId, userId)}`) as TAllPoints;
+export const createPoint = (body: IPointsList) => postCommon(body, '/points') as TPoint;
+export const updateSetOfPoints = (body: IPointsStatus[]) => patchCommon(body, '/points') as TAllPoints;
+export const getPointsByTaskId = (taskId: string) => getCommon(`/points/${taskId}`)as TAllPoints;
+export const updatePoint = (body: IPointsStatus) => patchCommon(body, '/points') as TPoint;
+export const deletePointById = (pointId: string) => deleteCommon(`/points/${pointId}`) as TPoint;
