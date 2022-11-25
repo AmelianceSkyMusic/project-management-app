4414f106b8e158f0574e7caa2529b8f3af492b37
Mazayw <mazayw@gmail.com>
2022-11-25 20:51:20 +0200

    refactor: users strict types

--- src/App/api/users.ts
+++ src/App/api/users.ts
@@ -4,7 +4,23 @@ import { deleteCommon } from './common/delete';
 import { getCommon } from './common/get';
 import { putCommon } from './common/put';
 
-export const getAllUsers = () => getCommon('/users');
-export const getUserById = (userId: string) => getCommon(`/users/${userId}`);
-export const updateUserById = (userId: string, body: IUser) => putCommon(body, `/users/${userId}`);
-export const deleteUserById = (userId: string) => deleteCommon(`/users/${userId}`);
+type TAllIUsers = Promise<{
+   data: IUser[] ;
+   status: number;
+} | {
+   data: null;
+   status: number | null;
+}>
+
+type TIUser = Promise<{
+   data: IUser ;
+   status: number;
+} | {
+   data: null;
+   status: number | null;
+}>
+
+export const getAllUsers = () => getCommon('/users') as TAllIUsers;
+export const getUserById = (userId: string) => getCommon(`/users/${userId}`) as TIUser;
+export const updateUserById = (userId: string, body: IUser) => putCommon(body, `/users/${userId}`)as TIUser;
+export const deleteUserById = (userId: string) => deleteCommon(`/users/${userId}`)as TIUser;
